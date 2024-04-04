dff51cd93af2d0c9d2afd87ffcb05a3c18326507
Madhurya Dutta <97863234+madhuryadutta@users.noreply.github.com>
2024-04-04 10:45:45 +0530

    Make minor changes in shell script for project installation and upgrade

---
+++ dockerupgrade.sh
@@ -0,0 +1,39 @@
+<<comment
+    |--------------------------------------------------------------------------
+    | Section 1: Docker Specific commands to Upgrade the Application
+    |--------------------------------------------------------------------------
+    |
+    |PLease scroll down and uncomment section 2 for plaform other than Docker
+    |
+comment
+
+#pulling latest code from git 
+# git stash
+git pull
+sudo chmod +x docker/entrypoint.sh
+
+# Docker image building
+sudo docker compose build --no-cache
+
+# Render Maintenance Page & Starting Maintenance mode with refresh every 15 sec
+sudo docker compose exec webserver php artisan down --render="errors::503" --refresh=15
+
+# container up (image already build)
+sudo docker compose up --build -d
+
+# running artisan command on the container named "webserver" 
+sudo docker compose exec webserver php artisan down --render="errors::503" --refresh=15
+sudo docker compose exec webserver composer install --optimize-autoloader --no-dev
+sudo docker compose exec webserver php artisan up
+echo "Application Upgrade Completed ............ "
+
+
+
+
+
+
+
+
+
+
+ 
--- upgrade.sh
+++ upgrade.sh
@@ -1,64 +1,24 @@
 <<comment
     |--------------------------------------------------------------------------
-    | Section 1: Docker Specific commands to Upgrade the Application
-    |--------------------------------------------------------------------------
-    |
-    |PLease scroll down and uncomment section 2 for plaform other than Docker
-    |
-comment
-
-# Render Maintenance Page & Starting Maintenance mode with refresh every 15 sec
-sudo docker compose exec webserver php artisan down --render="errors::503" --refresh=15
-
-#pulling latest code from git 
-# git stash
-git pull
-sudo chmod +x docker/entrypoint.sh
-# build container
-sudo docker compose up --build -d
-
-# running artisan command on the container named "webserver" 
-sudo docker compose exec webserver php artisan down --render="errors::503" --refresh=15
-sudo docker compose exec webserver composer install --optimize-autoloader --no-dev
-sudo docker compose exec webserver php artisan up
-echo "Application Upgrade Completed ............ "
-
-
-
-
-<<comment
-    |--------------------------------------------------------------------------
     | Section 1: Linux specific command to Upgrade the Application
     |--------------------------------------------------------------------------
     |
-    | uncomment the following codes 
+    | 
     |
 comment
 
-# php artisan down --render="errors::503" --refresh=15
-# git pull
-# php artisan config:clear
-# php artisan route:clear
-# php artisan view:clear
-# php artisan event:clear
-# composer install --optimize-autoloader --no-dev                                                                                            
-# php artisan config:cache
-# php artisan view:cache
-# php artisan route:cache
-# php artisan event:cache
-# php artisan about
-# php artisan route:list
-# php artisan config:show database
-# echo "Application is Upgrade Completed ............ "
-
-
-
-
-
-
-
-
-
-
-
- 
+php artisan down --render="errors::503" --refresh=15
+git pull
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan event:clear
+composer install --optimize-autoloader --no-dev                                                                                            
+php artisan config:cache
+php artisan view:cache
+php artisan route:cache
+php artisan event:cache
+php artisan about
+php artisan route:list
+php artisan config:show database
+echo "Application is Upgrade Completed ............ "
